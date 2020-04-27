bbacf2b27ee5c6b3d563ca3e898d5cff48fc22c0
minami <minaminaoy@gmail.com>
2020-04-28 06:18:45 +0900

    Add dijkstra

---
+++ src/GraphTheory/dijkstra.rs
@@ -0,0 +1,29 @@
+const INF_DIST: usize = std::usize::MAX;
+type Graph = Vec<Vec<(usize, usize, usize)>>;
+fn dijkstra(g: &Graph, start: usize) -> Vec<usize> {
+    let n = g.len();
+    let mut vis = vec![false; n];
+    let mut dist = vec![INF_DIST; n];
+    let mut heap = BinaryHeap::new();
+
+    dist[start] = 0;
+    heap.push(Reverse((0, start)));
+
+    while let Some(Reverse((cost, v))) = heap.pop() {
+        if dist[v] < cost {
+            continue;
+        }
+        vis[v] = true;
+        for e in &g[v] {
+            let (_, d, w) = &e;
+            if vis[*d] {
+                continue;
+            }
+            if dist[*d] > dist[v] + w {
+                dist[*d] = dist[v] + w;
+                heap.push(Reverse((dist[*d], *d)));
+            }
+        }
+    }
+    dist
+}
