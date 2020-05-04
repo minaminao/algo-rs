ca41becdec36585202fec14841deae1dd29a54a6
minami <minaminaoy@gmail.com>
2020-05-04 09:44:13 +0900

    Add get_divisors

---
+++ src/number_theory/divisors.rs
@@ -0,0 +1,17 @@
+fn get_divisors(x: usize) -> Vec<usize> {
+    let mut ret = Vec::new();
+    let mut i = 1;
+    while i * i < x {
+        if x % i != 0 {
+            i += 1;
+            continue;
+        }
+        ret.push(i);
+        ret.push(x / i);
+        i += 1;
+    }
+    if i * i == x {
+        ret.push(i);
+    }
+    ret
+}
