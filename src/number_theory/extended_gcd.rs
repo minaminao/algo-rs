7a89050c766613d3685623a81fe2d4ee6048c76b
minami <minaminaoy@gmail.com>
2020-08-15 22:06:28 +0900

    Add extgcd

---
+++ src/number_theory/extended_gcd.rs
@@ -0,0 +1,21 @@
+type T = i128;
+
+pub fn extgcd(a: T, b: T) -> (T, T, T) {
+    let mut g = a;
+    let mut x = 1;
+    let mut y = 0;
+    if b != 0 {
+        let (tg, ty, tx) = extgcd(b, a % b);
+        g = tg;
+        x = ty;
+        y = tx;
+        y -= a / b * x;
+    }
+    (g, x, y)
+}
+
+#[test]
+fn test() {
+    let (g, _x, _y) = extgcd(5, 10);
+    assert_eq!(g, 5);
+}
--- src/number_theory/mod.rs
+++ src/number_theory/mod.rs
@@ -1,3 +1,4 @@
 pub mod divisors;
+pub mod extended_gcd;
 pub mod gcd_lcm;
 pub mod miller_rabin_primality_test;
